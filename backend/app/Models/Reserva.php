b4360b891a74a803022d881587ec81bc8e94945f
Kevin <keving.kpg@gmail.com>
2025-04-21 15:38:43 -0500

    feat(api): Modelos para todos los objetos #5

---
+++ backend/app/Models/Reserva.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Reserva extends Model
+{
+    use HasFactory;
+
+    protected $table = 'reservas';
+    protected $primaryKey = 'id_reserva';
+    public $timestamps = false; // para simplicidad uso false para evitar la creación de columnas created_at y updated_at
+
+    protected $fillable = ['fecha', 'hora', 'numero_de_personas', 'id_comensal', 'id_mesa'];
+
+    public function comensal()
+    {
+        return $this->belongsTo(Comensal::class, 'id_comensal', 'id_comensal');
+    }
+
+    public function mesa()
+    {
+        return $this->belongsTo(Mesa::class, 'id_mesa', 'id_mesa'); 
+    }
+}
