6173513b43ff258488c302a3f48a8097607b3211
Kevin <keving.kpg@gmail.com>
2025-04-21 19:48:20 -0500

    Creación del front con Vue(Ts) e integración de Vuetify #17

---
+++ frontend/src/main.ts
@@ -0,0 +1,31 @@
+// main.ts
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles' 
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { createPinia } from 'pinia'
+
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+createApp(App)
+  .use(createPinia())
+  .use(vuetify)
+  .use(router)
+  .mount('#app')
---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+declare module 'vuetify/styles' {
+  const styles: string
+  export default styles
+}
+
+declare module 'vuetify/iconsets/mdi' {
+  import { IconSet } from 'vuetify'
+  export const mdi: IconSet
+  export const aliases: Record<string, string>
+}
